f7c88097d944503ddb9d4505d98838a2bed82179
lock <falsivates@gmail.com>
2021-06-15 16:36:34 +0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -27,16 +27,27 @@ $(document).ready(function () {
             });
 
             $slider.slick({
-                dots: false,
                 arrows: false,
+                dots: true,
                 speed: 500,
                 fade: true,
                 cssEase: 'linear',
+                customPaging: function (slick, index) {
+                    currentSlide = index + 1;
+                    currentSlide = currentSlide < 10 ? "0" + currentSlide : currentSlide;
+                    return '<a>' + currentSlide + '</a>';
+                },
                 prevArrow: $('.slick-proposal__prev'),
                 nextArrow: $('.slick-proposal__next'),
                 responsive: [{
+                    breakpoint: 1920,
+                    settings: {
+                        dots: false
+                    }
+                }, {
                     breakpoint: 767,
                     settings: {
+                        dots: false,
                         arrows: true,
                     }
                 }]
@@ -82,6 +93,15 @@ $(document).ready(function () {
         }]
     });
 
+    $(".proposal__pagination a").click(function (e) {
+        e.preventDefault();
+        $(".proposal__pagination a").removeClass('active');
+        $(this).addClass('active');
+        slideIndex = $(this).index();
+        console.log(slideIndex);
+        $('.proposal__slick').slick('slickGoTo', slideIndex);
+    });
+
     $(".hero__pagination a").click(function (e) {
         e.preventDefault();
         $(".hero__pagination a").removeClass('active');
@@ -96,37 +116,25 @@ $(document).ready(function () {
         dots: true,
         mobileFirst: true,
         responsive: [{
-                breakpoint: 1024,
-                settings: 'unslick'
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 0,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
+            breakpoint: 1024,
+            settings: 'unslick'
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
             }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
+        },
+        {
+            breakpoint: 0,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
         ]
     })
-
-    $(".proposal__pagination a").click(function (e) {
-        e.preventDefault();
-        $(".proposal__pagination a").removeClass('active');
-        $(this).addClass('active');
-        slideIndex = $(this).index();
-        console.log(slideIndex);
-        $('.proposal__slick').slick('slickGoTo', slideIndex);
-    });
 
     $('#burger').click(function () {
         $('#burger').toggleClass('active');
